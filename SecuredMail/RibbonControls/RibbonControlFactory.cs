4090ad87901fe699cd87c2036a5ccb67b1cd5788
nvsoftpro <vk165246@hotmail.com>
2016-12-05 15:24:53 +0200

    small refactoring

--- SecuredMail/RibbonControls/RibbonControlFactory.cs
+++ SecuredMail/RibbonControls/RibbonControlFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using SecuredMail.Logger;
 using SecuredMail.RibbonControls;
 using Office = Microsoft.Office.Core;
@@ -12,44 +13,73 @@ namespace SecuredMail
     class RibbonControlFactory
     {
         private ILogger logger;
-        private readonly Dictionary<int, IRibbonControlObject> controls;
+        private readonly Dictionary<int, List<IRibbonControlObject>> controls;
         public RibbonControlFactory()
         {
-            controls = new Dictionary<int, IRibbonControlObject>();
+            controls = new Dictionary<int, List<IRibbonControlObject>>();
         }
 
         public IRibbonControlObject GetControlObject(Office.IRibbonControl control)
         {
-            IRibbonControlObject controlObject = null;
+            List<IRibbonControlObject> controlObjects = null;
             if (control?.Context == null)
             {
                 return null;
             }
 
-            var controlObjectHashCode = control.Id.GetHashCode() + control.Context.GetHashCode();
-            Debug.WriteLine($"Context hash code:{controlObjectHashCode}");
-            logger.Message($"Context hash code:{controlObjectHashCode}");
-            controls.TryGetValue(controlObjectHashCode, out controlObject);
+            var contextHashCode = control.Context.GetHashCode();
+            Debug.WriteLine($"Context hash code:{contextHashCode}");
+            logger.Message($"Context hash code:{contextHashCode}");
+
+            IRibbonControlObject controlObject = null;
+            controls.TryGetValue(contextHashCode, out controlObjects);
+
+            if (controlObjects != null)
+            {
+                controlObject = controlObjects.FirstOrDefault(c => c.Tag == control.Tag);
+            }
+           
+
             if (controlObject == null)
             {
-                switch (control.Tag)
+                controlObject = ControlFactoryMethod(control);
+                if (controlObject != null)
                 {
-                    case "eratosthenes":
-                        controlObject = new ButtonEratosthenes(control.Id, control.Context, control.Tag);
-                        break;
-                    case "setsubject":
-                        controlObject = new ButtonSetObject(control.Id, control.Context, control.Tag);
-                        break;
-                    case "clearbody":
-                        controlObject = new ButtonClearBody(control.Id, control.Context, control.Tag);
-                        break;
-                    default:
-                        Trace.WriteLine("No control object. Make sure the tag of control is correct spelling");
-                        logger.Message("No control object. Make sure the tag of control is correct spelling");
-                        return null;
+                    
+                    if (controlObjects != null)
+                    {
+                        controlObjects.Add(controlObject);
+                        controls[contextHashCode] = controlObjects;
+                    }
+                    else
+                    {
+                        controlObjects = new List<IRibbonControlObject>(new[] {controlObject});
+                        controls.Add(contextHashCode, controlObjects);
+                    }
                 }
+            }
 
-                controls.Add(controlObjectHashCode, controlObject);
+            return controlObject;
+        }
+
+        private IRibbonControlObject ControlFactoryMethod(Office.IRibbonControl control)
+        {
+            IRibbonControlObject controlObject;
+            switch (control.Tag)
+            {
+                case "eratosthenes":
+                    controlObject = new ButtonEratosthenes(control.Id, control.Context, control.Tag);
+                    break;
+                case "setsubject":
+                    controlObject = new ButtonSetObject(control.Id, control.Context, control.Tag);
+                    break;
+                case "clearbody":
+                    controlObject = new ButtonClearBody(control.Id, control.Context, control.Tag);
+                    break;
+                default:
+                    Trace.WriteLine("No control object. Make sure the tag of control is correct spelling");
+                    logger.Message("No control object. Make sure the tag of control is correct spelling");
+                    return null;
             }
 
             return controlObject;
--- SecuredMail/Wrappers/Win32.cs
+++ SecuredMail/Wrappers/Win32.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Security;
 using System.Text;
 
 namespace SecuredMail.Wrappers
@@ -9,19 +10,19 @@ namespace SecuredMail.Wrappers
     /// </summary>
     public class Win32
     {
-        [DllImport("User32.dll")]
+        [DllImport("User32.dll"), SuppressUnmanagedCodeSecurity]
         public static extern Int32 FindWindow(String lpClassName, String lpWindowName);
 
-        [DllImport("User32.dll")]
+        [DllImport("User32.dll"), SuppressUnmanagedCodeSecurity]
         public static extern Int32 SetForegroundWindow(int hWnd);
 
-        [DllImport("User32.dll")]
+        [DllImport("User32.dll"), SuppressUnmanagedCodeSecurity]
         public static extern Boolean EnumChildWindows(int hWndParent, Delegate lpEnumFunc, int lParam);
 
-        [DllImport("User32.dll")]
+        [DllImport("User32.dll"), SuppressUnmanagedCodeSecurity]
         public static extern Int32 GetWindowText(int hWnd, StringBuilder s, int nMaxCount);
 
-        [DllImport("User32.dll")]
+        [DllImport("User32.dll"), SuppressUnmanagedCodeSecurity]
         public static extern Int32 GetWindowTextLength(int hwnd);
     }
 }
